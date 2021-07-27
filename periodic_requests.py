f04cc1aaddc18a51a5e35c84ffa1276d880d5baf
ElDonatello <47677152+ElDonatello@users.noreply.github.com>
2021-07-27 12:23:06 +0100

    Add files via upload

---
+++ periodic_requests.py
@@ -0,0 +1,18 @@
+import requests
+import pytz
+from apscheduler.schedulers.blocking import BlockingScheduler
+from twisted.internet import reactor
+
+
+def send_request():
+    requests.post("https://calm-everglades-62471.herokuapp.com/schedule.json", data={
+        'project': 'default',
+        'spider': 'ouedkniss'
+    })
+
+
+if __name__ == '__main__':
+        scheduler = BlockingScheduler(timezone="Africa/Lagos")
+        scheduler.add_job(send_request,'cron', hour = '11', minute = '32')
+        scheduler.start()
+        reactor.run()

189214b5c6a56d7a3eb6ad63cdcd925f8395506b
ElDonatello <47677152+ElDonatello@users.noreply.github.com>
2021-08-17 16:34:47 +0100

    Update periodic_requests.py

--- periodic_requests.py
+++ periodic_requests.py
@@ -13,6 +13,6 @@ def send_request():
 
 if __name__ == '__main__':
         scheduler = BlockingScheduler(timezone="Africa/Lagos")
-        scheduler.add_job(send_request,'cron', hour = '16', minute = '32')
+        scheduler.add_job(send_request,'cron', hour = '16', minute = '36')
         scheduler.start()
         reactor.run()

47f01eb4cd432ee2c02e1b7656a3245e6b79dd08
ElDonatello <47677152+ElDonatello@users.noreply.github.com>
2021-08-22 19:10:50 +0100

    Update periodic_requests.py

--- periodic_requests.py
+++ periodic_requests.py
@@ -13,8 +13,8 @@ def send_request():
     requests.get("https://sayarti.herokuapp.com/")
 
 if __name__ == '__main__':
-        schedule.every().day.at("18:05").do(send_request)
-        schedule.every().day.at("20:00").do(send_request)
+        schedule.every().day.at("19:10").do(send_request)
+        schedule.every().day.at("20:00").do(send_request2)
         reactor.run()
         while (true):
             schedule.run_pending()

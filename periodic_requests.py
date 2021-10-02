9d75cf7afcce640dbdcbbbd22b6149d134555d69
AmineBechar96 <47677152+AmineBechar96@users.noreply.github.com>
2021-10-02 18:10:16 +0100

    Update periodic_requests.py

--- periodic_requests.py
+++ periodic_requests.py
@@ -5,24 +5,15 @@ from twisted.internet import reactor
 
 
 def send_request():
-    
+    requests.get("https://sayarti3.herokuapp.com"}
     requests.post("https://scrapya.herokuapp.com/schedule.json", data={
         'project': 'default',
         'spider': 'ouedkniss'
     })
-    
- def send_request2():
-    
-    requests.post("https://scrapya.herokuapp.com/schedule.json", data={
-        'project': 'default',
-        'spider': 'ouedkniss'
-    })
+ 
 
 if __name__ == '__main__':
         scheduler = BlockingScheduler(timezone="Africa/Lagos")
-        scheduler2 = BlockingScheduler(timezone="Africa/Lagos")
-        scheduler.add_job(send_request,'cron', hour = '18', minute = '03')
-        scheduler2.add_job(send_request2,'cron', hour = '18', minute = '04')
+        scheduler.add_job(send_request,'cron', hour = '01', minute = '23')
         scheduler.start()
-        scheduler2.start()
         reactor.run()

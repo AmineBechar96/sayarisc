d81e5dbb85c9594b27f4c4a1302260063004cd4d
AmineBechar96 <47677152+AmineBechar96@users.noreply.github.com>
2021-10-02 18:00:32 +0100

    Update periodic_requests.py

--- periodic_requests.py
+++ periodic_requests.py
@@ -21,8 +21,8 @@ def send_request():
 if __name__ == '__main__':
         scheduler = BlockingScheduler(timezone="Africa/Lagos")
         scheduler2 = BlockingScheduler(timezone="Africa/Lagos")
-        scheduler.add_job(send_request,'cron', hour = '17', minute = '58')
-        scheduler2.add_job(send_request2,'cron', hour = '17', minute = '59')
+        scheduler.add_job(send_request,'cron', hour = '18', minute = '03')
+        scheduler2.add_job(send_request2,'cron', hour = '18', minute = '04')
         scheduler.start()
         scheduler2.start()
         reactor.run()
